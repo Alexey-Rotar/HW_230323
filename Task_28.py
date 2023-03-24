60acc50a4abcade92c6c03a1993b6a5cdffd895f
Alexey <121248248+Alexey-Rotar@users.noreply.github.com>
2023-03-24 10:35:23 +0500

    Task_28 added optimization_2

--- Task_28.py
+++ Task_28.py
@@ -6,9 +6,13 @@ def sum_function(a, b):
         return b
     elif b == 0:
         return a
-    else:
+    elif a <= b:
         a -= 1
         b += 1
+        return sum_function(a, b)
+    else:
+        b -= 1
+        a += 1
         return sum_function(a, b)
 
 a = int(input("Введите первое число: "))
